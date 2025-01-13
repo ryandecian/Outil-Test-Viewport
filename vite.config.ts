bf0c4c3fbc8e3765296246fa9c2bd82b948aedbe
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-13 17:37:58 +0100

    Configuration de vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // Permet d'écouter sur toutes les interfaces réseau
+    port: 3000, // Ou tout autre port que vous préférez
+  },
 })
