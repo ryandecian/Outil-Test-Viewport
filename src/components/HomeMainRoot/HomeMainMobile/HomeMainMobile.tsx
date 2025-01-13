c06d842a2ccc9cf752766186e080cf90f3d31902
Ryan DECIAN <ryan.decian.propgmail.com>
2025-01-13 20:55:52 +0100

    Création partiel de la logique de HomeMainRoot

--- src/components/HomeMainRoot/HomeMainMobile/HomeMainMobile.tsx
+++ src/components/HomeMainRoot/HomeMainMobile/HomeMainMobile.tsx
@@ -1,7 +1,37 @@
 import "./HomeMainMobile.css"
+import { useState } from "react";
+import { useEffect } from "react";
 
 function HomeMainMobile() {
-    return ()
+     /*Logique de calcul et récupération des dimensions écran*/
+     // État pour les dimensions de l'écran
+     const [dimensions, setDimensions] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        });
+  
+   // Mettre à jour les dimensions de l'écran lors du redimensionnement
+   useEffect(() => {
+    const handleResize = () => {
+      setDimensions({
+        width: window.innerWidth,
+        height: window.innerHeight,
+        });
+    };
+
+window.addEventListener("resize", handleResize);
+
+// Nettoyage
+  return () => {
+    window.removeEventListener("resize", handleResize);
+  };
+}, []);
+    return (
+        <>
+             <p>{`Width = ${dimensions.width}px`}</p>
+             <p></p>
+        </>
+    )
 }
 
 export default HomeMainMobile;
